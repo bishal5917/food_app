f818e5ba92febf017bf3a0793a3ec14c47362f3a
bishal5917 <poudelb172@gmail.com>
2022-10-07 19:55:27 +0545

    category model made

---
+++ lib/models/category.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final String id;
+  final String title;
+  final Color color;
+
+  const Category(
+      {this.id = "co", this.title = "default", this.color = Colors.orange});
+}
