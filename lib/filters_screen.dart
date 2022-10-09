a07661ea2fa8a8c08cbc311be508dfbc4c2c88ad
bishal5917 <poudelb172@gmail.com>
2022-10-09 19:04:31 +0545

    working with filters

--- lib/filters_screen.dart
+++ lib/filters_screen.dart
@@ -1,14 +1,62 @@
 import 'package:flutter/material.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool glutenFree = false;
+  bool lactoseFree = false;
+  bool vegan = false;
+  bool vegetarian = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Apply Filters")),
-      body: Center(
-        child: Text("OK"),
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView(
+            children: [
+              SwitchListTile(
+                  title: Text('Gluten Free'),
+                  value: glutenFree,
+                  onChanged: (newVal) {
+                    setState(() {
+                      glutenFree = newVal;
+                    });
+                  }),
+              SwitchListTile(
+                  title: Text('Lactose Free'),
+                  value: lactoseFree,
+                  onChanged: (newVal) {
+                    setState(() {
+                      lactoseFree = newVal;
+                    });
+                  }),
+              SwitchListTile(
+                  title: Text('Vegan'),
+                  value: vegan,
+                  onChanged: (newVal) {
+                    setState(() {
+                      vegan = newVal;
+                    });
+                  }),
+              SwitchListTile(
+                  title: Text('Vegetarian'),
+                  value: vegetarian,
+                  onChanged: (newVal) {
+                    setState(() {
+                      vegetarian = newVal;
+                    });
+                  })
+            ],
+          ))
+        ],
       ),
     );
   }
--- lib/meal_detail_screen.dart
+++ lib/meal_detail_screen.dart
@@ -10,75 +10,80 @@ class MealDetailScreen extends StatelessWidget {
     final mealDetail = DUMMY_MEALS.firstWhere((mel) => mel.id == mealId);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(mealDetail.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              height: 300,
-              width: double.infinity,
-              child: Image.network(
-                mealDetail.imageUrl,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                "Ingredients",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              height: 300,
-              width: 350,
-              child: ListView.builder(
-                itemCount: mealDetail.ingredients.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    color: Color.fromARGB(255, 216, 156, 151),
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        mealDetail.ingredients[index],
-                        style: TextStyle(fontSize: 17),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                "STEPS",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              height: 300,
-              width: 350,
-              child: ListView.builder(
-                itemCount: mealDetail.steps.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: CircleAvatar(
-                        child: Text("#${index + 1}"),
-                        backgroundColor: Color.fromARGB(255, 231, 229, 229)),
-                    title: Text(
-                      mealDetail.steps[index],
-                      style: TextStyle(fontFamily: 'baloo'),
-                    ),
-                  );
-                },
-              ),
-            )
-          ],
+        appBar: AppBar(
+          title: Text(mealDetail.title),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  mealDetail.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  "Ingredients",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                height: 300,
+                width: 350,
+                child: ListView.builder(
+                  itemCount: mealDetail.ingredients.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: Color.fromARGB(255, 216, 156, 151),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          mealDetail.ingredients[index],
+                          style: TextStyle(fontSize: 17),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  "STEPS",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                height: 300,
+                width: 350,
+                child: ListView.builder(
+                  itemCount: mealDetail.steps.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                          child: Text("#${index + 1}"),
+                          backgroundColor: Color.fromARGB(255, 231, 229, 229)),
+                      title: Text(
+                        mealDetail.steps[index],
+                        style: TextStyle(fontFamily: 'baloo'),
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ));
   }
 }
